523aec70a19313f6430192f9d5dc0a67982c4ec3
Heonhyo Lee <lhh-znso4@gm.gist.ac.kr>
2023-10-08 00:14:37 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -59,10 +59,10 @@ const CONFIG = {
     },
   },
   cusdis: {
-    enable: false,
+    enable: true,
     config: {
       host: "https://cusdis.com",
-      appid: "", // Embed Code -> data-app-id value
+      appid: "e55dc05f-3220-4e5a-80a9-1d969c07fed8", // Embed Code -> data-app-id value
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
